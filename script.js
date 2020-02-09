267b2fabd17f132239e538883b516f3de4701f24
JunXingLiu <35229076+JunXingLiu@users.noreply.github.com>
2020-02-09 16:14:57 -0700

    added comments

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ const messageForm = document.getElementById("send-container")
 const messageContainer = document.getElementById("message-container")
 const messageInput = document.getElementById("message-input")
 
+// get the name of the new user
 const name = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
 
+// sending the message to all
 socket.on('chat-message', data => {
     appendMessage(`${data.name}: ${data.message}`)
 })
 
+// notify a new user has joined
 socket.on('user-connected', name => {
     appendMessage(`${name} connected`)
 })
 
+// notify user has disconnected
 socket.on('user-disconnected', data => {
     appendMessage(`${name} disconnected`)
 })
 
+// prevent refresh when hit submit
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
@@ -27,6 +32,7 @@ messageForm.addEventListener('submit', e => {
     messageInput.value = ''
 })
 
+// create new message div
 function appendMessage(message) {
     const messageElement = document.createElement('div')
     messageElement.innerText = message
--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+// require
 const io = require('socket.io')(3000)
 
+// list of users
 const users = {}
 
 io.on('connection', socket => {
+    // new user
     socket.on('new-user', name => {
         users[socket.id] = name
         socket.broadcast.emit('user-connected', name)
     })
+    // sending message to all
     socket.on('send-chat-message', message => {
         socket.broadcast.emit('chat-message', { message: message, name: users[socket.id]})
     })
+    // disconnect and clearing from list
     socket.on('disconnect', () => {
         socket.broadcast.emit('user-disconnected', users[socket.id])
         delete users[socket.id]
